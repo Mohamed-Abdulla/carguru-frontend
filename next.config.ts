483ce12edc3d36d8aecbbcc15dfb3f709ee40c1b
Mohamed Abdulla <abdabdulla78@gmail.com>
2026-04-19 15:40:27 +0530

    commented output sand alone

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const isVercel = process.env.VERCEL === "1";
 
 const nextConfig: NextConfig = {
-  ...(isVercel ? {} : { output: "standalone" }),
+  // ...(isVercel ? {} : { output: "standalone" }),
 };
 
 export default nextConfig;
