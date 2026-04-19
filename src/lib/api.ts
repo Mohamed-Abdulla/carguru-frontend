b36e4e604e21845776beb33a4c47dc4a5fc43ced
Mohamed Abdulla <abdabdulla78@gmail.com>
2026-04-19 15:51:38 +0530

    vercel https workaround

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,6 @@
 import { AnalyticsStats, Car, RecommendResult, RecommendationPreferences } from "./types";
 
-console.log(process.env.NEXT_PUBLIC_API_URL)
-const API_BASE = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3003";
+const API_BASE = process.env.NEXT_PUBLIC_API_URL || "";
 
 async function apiFetch<T>(path: string, init?: RequestInit): Promise<T> {
   const res = await fetch(`${API_BASE}${path}`, {
